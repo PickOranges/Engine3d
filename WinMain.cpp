766329004c692823babb18f287be311e47d3bdc2
chengzimdl@gmail.com <chengzimdl@gmail.com>
2021-12-17 11:43:25 +0100

    clean

--- WinMain.cpp
+++ WinMain.cpp
@@ -15,12 +15,10 @@ int CALLBACK WinMain(
 	BOOL gResult;
 	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
-		// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	// check if GetMessage call itself borked
 	if (gResult == -1)
 	{
 		return -1;

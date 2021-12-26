e451df22d7de1337486e9e09c14edee8f192bc00
chengzimdl@gmail.com <chengzimdl@gmail.com>
2021-12-26 18:15:31 +0100

    update

--- Graphics.cpp
+++ Graphics.cpp
@@ -99,16 +99,87 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 
 void Graphics::DrawTestTriangle()
 {
+	using namespace std;
+	vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+	vector<vector<float>> temp_vertices;
+	vector<vector<float>> temp_uvs;
+	vector<vector<float>> temp_normals;
+
+	FILE* file = fopen(" ", "r");
+	if (file == NULL) {
+		printf("Impossible to open the file !\n");
+		return;
+	}
+
+	while (1) {
+
+		char lineHeader[128];
+		// read the first word of the line
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File. Quit the loop.
+
+		// else : parse lineHeader
+		if (strcmp(lineHeader, "v") == 0) {
+			vector<float> vertex(3, 0);
+			fscanf(file, "%f %f %f\n", &vertex[0], &vertex[1], &vertex[2]);
+			temp_vertices.push_back(vertex);
+		}
+		else if (strcmp(lineHeader, "vt") == 0) {
+			vector<float> uv;
+			fscanf(file, "%f %f\n", &uv[0], &uv[1]);
+			temp_uvs.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0) {
+			vector<float> normal;
+			fscanf(file, "%f %f %f\n", &normal[0], &normal[1], &normal[2]);
+			temp_normals.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			if (matches != 9) {
+				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+				return;
+			}
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+			uvIndices.push_back(uvIndex[0]);
+			uvIndices.push_back(uvIndex[1]);
+			uvIndices.push_back(uvIndex[2]);
+			normalIndices.push_back(normalIndex[0]);
+			normalIndices.push_back(normalIndex[1]);
+			normalIndices.push_back(normalIndex[2]);
+		}
+	}
+
+
+
 	HRESULT hr;
+	//struct Vertex {
+	//	float x;
+	//	float y;
+	//};
+	//const Vertex vertices[] = {
+	//	{ 0.0f,0.5f },
+	//	{ 0.5f,-0.5f },
+	//	{ -0.5f,-0.5f }
+	//};
 	struct Vertex {
 		float x;
 		float y;
+		float z;
 	};
-	const Vertex vertices[] = {
-		{ 0.0f,0.5f },
-		{ 0.5f,-0.5f },
-		{ -0.5f,-0.5f }
-	};
+
+	const int vlen = sizeof(temp_vertices);
+	Vertex vertices[vlen];
+	for (const auto& p:temp_vertices) {
+		vertices += {p[0], p[1], p[2]};
+	}
+
+
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;

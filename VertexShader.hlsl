056d83a101f4e429204e40ea5a00b9a2882d34ee
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-01-04 12:53:56 +0100

    pass transform from cpu to gpu, in order to rotate the object.
    create constant buffer for this TF on gpu, and bind it on vertex shader, so that every vertex can use it.

--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -3,10 +3,17 @@ struct VSOut {
 	float4 pos:SV_Position;
 };
 
+
+cbuffer CBuf
+{
+	row_major matrix transform;
+};
+
 VSOut main( float2 pos : POSITION, float3 col:Color )
 {
 	VSOut vso;
-	vso.pos = float4(pos,0.0f,1.0f);
+	//vso.pos = float4(pos,0.0f,1.0f);
+	vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
 	vso.col = col;
 	return vso;
 } 

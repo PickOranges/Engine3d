dbbc8229b6a62a1fc67ae543389880f5ffa56292
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-01-07 15:45:56 +0100

    Add boxes into App, draw multiple boxes with random number.

--- App.h
+++ App.h
@@ -7,10 +7,12 @@ class App
 public:
 	App();
 	int Go();
+	~App();
 private:
 	void DoFrame(); // contain top-level game logic.
 private:
 	Window wnd;
 	TimerWrapper timer;
+	std::vector<std::unique_ptr<class Box>> boxes;
 };
 

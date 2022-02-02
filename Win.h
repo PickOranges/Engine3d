30c709def0bd3266a07cb45d616814720b78eba7
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-02-02 10:17:31 +0100

    update

--- Win.h
+++ Win.h
@@ -3,6 +3,7 @@
 #define _WIN32_WINNT 0x0601
 #include <sdkddkver.h>
 
+#ifndef FULL_WINTARD
 #define WIN32_LEAN_AND_MEAN
 #define NOGDICAPMASKS
 #define NOSYSMETRICS
@@ -20,7 +21,7 @@
 #define NONLS
 #define NOMEMMGR
 #define NOMETAFILE
-#define NOMINMAX
+//#define NOMINMAX
 #define NOOPENFILE
 #define NOSCROLL
 #define NOSERVICE
@@ -37,24 +38,26 @@
 #define NOPROXYSTUB
 #define NOIMAGE
 #define NOTAPE
+#endif
 
+
+
+#define NOMINMAX
 #define STRICT
 
 #include <Windows.h>
-#include <stdlib.h>
-
-
-extern inline LPCWSTR convchar(const char* csrc) {
-	wchar_t* dest = new wchar_t[2000];
-	size_t len = strlen(csrc) + 1;
-	//mbstowcs_s(&len, dest, 100, csrc, 300);
-	mbstowcs(dest,csrc,len);
-	return dest;
-}
-
-extern inline char* convwchar(const wchar_t* wsrc) {
-	char* dest = new char[2000];
-	size_t len = wcslen(wsrc) + 1;
-	wcstombs(dest, wsrc, len);
-	return dest;
-}
+//#include <stdlib.h>
+//extern inline LPCWSTR convchar(const char* csrc) {
+//	wchar_t* dest = new wchar_t[2000];
+//	size_t len = strlen(csrc) + 1;
+//	//mbstowcs_s(&len, dest, 100, csrc, 300);
+//	mbstowcs(dest,csrc,len);
+//	return dest;
+//}
+//
+//extern inline char* convwchar(const wchar_t* wsrc) {
+//	char* dest = new char[2000];
+//	size_t len = wcslen(wsrc) + 1;
+//	wcstombs(dest, wsrc, len);
+//	return dest;
+//}

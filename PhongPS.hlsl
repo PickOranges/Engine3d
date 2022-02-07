3b9d8d33d0a04502aa7a3933d08255d55e1b87e9
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-02-07 15:33:33 +0100

    Add ImGui slider bar to control light source.

--- PhongPS.hlsl
+++ PhongPS.hlsl
@@ -1,15 +1,15 @@
 cbuffer LightCBuf
 {
 	float3 lightPos;
+	float3 materialColor;
+	float3 ambient;
+	float3 diffuseColor;
+	float diffuseIntensity;
+	float attConst;
+	float attLin;
+	float attQuad;
 };
 
-static const float3 materialColor = { 0.25f,0.0f,1.0f };
-static const float3 ambient = { 0.05f,0.05f,0.05f };
-static const float3 diffuseColor = { 1.0f,1.0f,1.0f };
-static const float diffuseIntensity = 1.0f;
-static const float attConst = 1.0f;
-static const float attLin = 0.015f;
-static const float attQuad = 0.00125f;
 
 float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
 {

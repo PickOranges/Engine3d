5dda616f806f8befab722cf4578fe91662163319
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-02-07 13:18:15 +0100

    Fix light source const buffer issue.

--- Box.cpp
+++ Box.cpp
@@ -38,22 +38,13 @@ Box::Box(Graphics& gfx,
 
 		AddStaticBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
 
-		//auto pvs = std::make_unique<VertexShader>(gfx, L"ColorIndexVS.cso");
 		auto pvs = std::make_unique<VertexShader>(gfx, L"PhongVS.cso");
 		auto pvsbc = pvs->GetBytecode();
 		AddStaticBind(std::move(pvs));
 
-		//AddStaticBind(std::make_unique<PixelShader>(gfx, L"ColorIndexPS.cso"));
 		AddStaticBind(std::make_unique<PixelShader>(gfx, L"PhongPS.cso"));
 
 		AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, model.indices));
-
-		struct PSLightConstants
-		{
-			dx::XMVECTOR pos;
-		};
-		//AddStaticBind(std::make_unique<PixelConstantBuffer<PixelShaderConstants>>(gfx, cb2));
-		AddStaticBind(std::make_unique<PixelConstantBuffer<PSLightConstants>>(gfx));
 
 		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
 		{
--- PhongPS.hlsl
+++ PhongPS.hlsl
@@ -4,13 +4,12 @@ cbuffer LightCBuf
 };
 
 static const float3 materialColor = { 0.7f,0.7f,0.9f };
-static const float3 ambient = { 0.15f,0.15f,0.15f };
-//static const float3 diffuseColor = { 1.0f,1.0f,1.0f };
-static const float3 diffuseColor = { 0.54f,0.0f,1.0f };
+static const float3 ambient = { 0.05f,0.05f,0.05f };
+static const float3 diffuseColor = { 0.3f,0.0f,1.0f };
 static const float diffuseIntensity = 1.0f;
 static const float attConst = 1.0f;
-static const float attLin = 1.0f;
-static const float attQuad = 1.0f;
+static const float attLin = 0.045f;
+static const float attQuad = 0.0075f;
 
 float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
 {

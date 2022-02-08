92198ce7d7ad86aecc23c5d5c85322844edf39a0
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-02-08 17:36:57 +0100

    update.

--- App.cpp
+++ App.cpp
@@ -9,6 +9,9 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_impl_dx11.h"
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
 
 
 App::App()

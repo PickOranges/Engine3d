7e622b3fde7febd922bb00b28bc6da7baf4ac702
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-02-14 21:39:02 +0100

    Adjust App class.

--- App.cpp
+++ App.cpp
@@ -1,15 +1,10 @@
 #include "App.h"
-//#include "Melon.h"
 #include "Pyramid.h"
 #include "Box.h"
 #include "Cylinder.h"
 #include <memory>
 #include <algorithm>
 #include "SimpleMath.h"
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/imgui_impl_dx11.h"
-#include "AssTest.h"
 
 
 App::App()
@@ -17,77 +12,7 @@ App::App()
 	wnd(800, 600, "Test App Class Obj"),
 	light(wnd.Gfx())
 {
-	class Factory
-	{
-	public:
-		Factory(Graphics& gfx)
-			:
-			gfx(gfx)
-		{}
-		std::unique_ptr<Drawable> operator()()
-		{
-			const DirectX::XMFLOAT3 mat = { cdist(rng),cdist(rng),cdist(rng) };
-			//const DirectX::XMFLOAT3 mat = { 0.5f, 0.5f, 1.0f }; // single color for all objects, it is convenient for debugging.
-
-			switch (sdist(rng))
-			//switch (3)
-			{
-			case 0:
-				return std::make_unique<Box>(
-					gfx, rng, adist, ddist,
-					odist, rdist, bdist, mat
-					);
-			case 1:
-				return std::make_unique<Cylinder>(
-					gfx, rng, adist, ddist, odist,
-					rdist, bdist, tdist
-					);
-			case 2:
-				return std::make_unique<Pyramid>(
-					gfx, rng, adist, ddist, odist,
-					rdist, tdist
-					);
-			case 3:
-				return std::make_unique<AssTest>(
-					gfx, rng, adist, ddist,
-					odist, rdist, mat, 0.7f
-					);
-			default:
-				assert(false && "impossible drawable option in factory");
-				return {};
-			}
-		}
-	private:
-		Graphics& gfx;
-		std::mt19937 rng{ std::random_device{}() };
-		std::uniform_int_distribution<int> sdist{ 0,3 };
-		std::uniform_real_distribution<float> adist{ 0.0f,PI * 2.0f };
-		std::uniform_real_distribution<float> ddist{ 0.0f,PI * 0.5f };
-		std::uniform_real_distribution<float> odist{ 0.0f,PI * 0.08f };
-		std::uniform_real_distribution<float> rdist{ 6.0f,20.0f };
-		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
-		std::uniform_real_distribution<float> cdist{ 0.0f,1.0f };
-
-		std::uniform_int_distribution<int> latdist{ 5,20 };
-		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
-
-		std::uniform_int_distribution<int> tdist{ 3,30 };
-	};
-
-	Factory f(wnd.Gfx());
-	drawables.reserve(nDrawables);
-	std::generate_n(std::back_inserter(drawables), nDrawables, f);
-	// init box pointers for editing instance parameters
-	for (auto& pd : drawables)
-	{
-		if (auto pb = dynamic_cast<Box*>(pd.get()))
-		{
-			boxes.push_back(pb);
-		}
-	}
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
-
 }
 
 int App::Go()
@@ -117,90 +42,13 @@ void App::DoFrame()
 	// Now update camera params every frame(Instead of updating just once in ctor when init App).
 	wnd.Gfx().SetCamera(cam.GetMatrix());
 	light.Bind(wnd.Gfx(), cam.GetMatrix());
-
-	for (auto& d : drawables)
-	{
-		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
-		d->Draw(wnd.Gfx());
-	}
 	light.Draw(wnd.Gfx()); // draw the light source lastely.
+	nano.Draw(wnd.Gfx());
 	
 	// imgui windows
-	SpawnSimulationWindow();
 	cam.SpawnControlWindow();
 	light.SpawnControlWindow();
-	SpawnBoxWindowManagerWindow();
-	SpawnBoxWindows();
-
 
 	// present
 	wnd.Gfx().EndFrame();
-}
-
-void App::SpawnSimulationWindow() noexcept
-{
-	if (ImGui::Begin("Simulation Speed"))
-	{
-		//ImGui::ShowDemoWindow(&show_demo_window);
-		ImGui::SliderFloat("Speed Factor", &speed_factor, 0.0f, 6.0f);
-		ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::Text("Status: %s", wnd.kbd.KeyIsPressed(VK_SPACE) ? "PAUSED" : "RUNNING(Hole the SPACE to pause)");
-	}
-	ImGui::End();
-}
-
-void App::SpawnBoxWindowManagerWindow() noexcept
-{
-	// imgui window to open box windows
-	if (ImGui::Begin("Boxes"))
-	{
-		using namespace std::string_literals;
-		const auto preview = comboBoxIndex.has_value() ? std::to_string(comboBoxIndex.value()) : "Choose a box..."s;
-		if (ImGui::BeginCombo("Box Number", preview.c_str()))
-		{
-			// If comboBoxIndex is not empty, then select that box, otherwise do nothing.
-			if (comboBoxIndex.has_value()) {
-				for (size_t i = 0; i < boxes.size(); i++)
-				{
-					const bool selected = *comboBoxIndex == i;
-					if (ImGui::Selectable(std::to_string(i).c_str(), selected))
-					{
-						comboBoxIndex = i;
-					}
-					if (selected)
-					{
-						ImGui::SetItemDefaultFocus();
-					}
-				}
-			}
-			else {
-				// Add this line to fix accessing empty optional issue and cannot re-select issue.
-				if (!boxes.empty()) comboBoxIndex = 0; 
-			}
-				
-			ImGui::EndCombo();
-		}
-		if (ImGui::Button("Spawn Control Window") && comboBoxIndex)
-		{
-			boxControlIds.insert(*comboBoxIndex);
-			comboBoxIndex.reset();
-		}
-	}
-	ImGui::End();
-}
-
-void App::SpawnBoxWindows() noexcept
-{
-	// imgui box attribute control windows
-	for (auto i = boxControlIds.begin(); i != boxControlIds.end(); )
-	{
-		if (!boxes[*i]->SpawnControlWindow(*i, wnd.Gfx()))
-		{
-			i = boxControlIds.erase(i);
-		}
-		else
-		{
-			i++;
-		}
-	}
 }
--- App.h
+++ App.h
@@ -5,6 +5,7 @@
 #include "Camera.h"
 #include "PointLight.h"
 #include <set>
+#include "Mesh.h"
 
 class App
 {
@@ -15,20 +16,14 @@ public:
 	~App();
 private:
 	void DoFrame(); // contain top-level game logic.
-	void SpawnSimulationWindow() noexcept;
-	void SpawnBoxWindowManagerWindow() noexcept;  // For box-selection-dropdown menu.
-	void SpawnBoxWindows() noexcept; // For changing the material params of the selected box.
+
 private:
 	ImguiManager imgui;
 	Window wnd;
 	TimerWrapper timer;
 	Camera cam;
 	PointLight light;
-	std::vector<class Box*> boxes;
-	std::vector<std::unique_ptr<class Drawable>> drawables;
-	static constexpr size_t nDrawables = 180;
 	float speed_factor = 1.0f;
-	std::optional<int> comboBoxIndex;
-	std::set<int> boxControlIds;
+	Model nano{ wnd.Gfx(),"models\\nanosuit.obj" };
 };
 

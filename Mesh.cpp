b5f696d93c0fee786e1139cacf1fd5b5eddf271d
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-18 10:38:46 +0100

    3rd try

--- Mesh.cpp
+++ Mesh.cpp
@@ -250,6 +250,7 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 	std::vector<std::unique_ptr<Bindable>> bindablePtrs;
 
 	bool hasSpecularMap = false;
+	float shininess = 35.0f;
 	if (mesh.mMaterialIndex >= 0)
 	{
 		auto& material = *pMaterials[mesh.mMaterialIndex];
@@ -266,6 +267,11 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 			bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx, Surface::FromFile(base + texFileName.C_Str()), 1));
 			bool hasSpecularMap = true;
 		}
+		else
+		{
+			material.Get(AI_MATKEY_SHININESS, shininess);
+		}
+
 		bindablePtrs.push_back(std::make_unique<Bind::Sampler>(gfx));
 	}
 
@@ -288,12 +294,15 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 		bindablePtrs.push_back(std::make_unique<Bind::PixelShader>(gfx, L"PhongPSSpecMap.cso"));
 	}
 	else {
+
 		struct PSMaterialConstant
 		{
 			float specularIntensity = 0.8f;
-			float specularPower = 40.0f;
+			float specularPower;
 			float padding[2];
 		} pmc;
+		pmc.specularPower = shininess;
+
 		bindablePtrs.push_back(std::make_unique<PixelConstantBuffer<PSMaterialConstant>>(gfx, pmc, 1u));
 	}
 

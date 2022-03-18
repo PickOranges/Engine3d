59880a37f4bf8c1c584b7a95a654e996c4ccd586
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-18 08:50:25 +0100

    test new Surface class(based on DxTex) inside Model::ParseMesh(...)

--- Mesh.cpp
+++ Mesh.cpp
@@ -223,24 +223,16 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 		VertexLayout{}
 		.Append(VertexLayout::Position3D)
 		.Append(VertexLayout::Normal)
-		//.Append(VertexLayout::Texture2D)
+		.Append(VertexLayout::Texture2D)
 	));
-
-
-	auto& material = *pMaterials[mesh.mMaterialIndex];
-	for (int i = 0; i < material.mNumProperties; i++)
-	{
-		auto& prop = *material.mProperties[i];
-		int qqq = 90;
-	}
 
 
 	for (unsigned int i = 0; i < mesh.mNumVertices; i++)
 	{
 		vbuf.EmplaceBack(
 			*reinterpret_cast<dx::XMFLOAT3*>(&mesh.mVertices[i]),
-			*reinterpret_cast<dx::XMFLOAT3*>(&mesh.mNormals[i])
-			//*reinterpret_cast<dx::XMFLOAT2*>(&mesh.mTextureCoords[0][i])
+			*reinterpret_cast<dx::XMFLOAT3*>(&mesh.mNormals[i]),
+			*reinterpret_cast<dx::XMFLOAT2*>(&mesh.mTextureCoords[0][i])
 		);
 	}
 
@@ -257,15 +249,15 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 
 	std::vector<std::unique_ptr<Bindable>> bindablePtrs;
 
-	//if (mesh.mMaterialIndex >= 0)
-	//{
-	//	using namespace std::string_literals;
-	//	auto& material = *pMaterials[mesh.mMaterialIndex];
-	//	aiString texFileName;
-	//	material.GetTexture(aiTextureType_DIFFUSE, 0, &texFileName);
-	//	bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx, Surface::FromFile("Models\\nano_textured\\"s + texFileName.C_Str())));
-	//	bindablePtrs.push_back(std::make_unique<Bind::Sampler>(gfx));
-	//}
+	if (mesh.mMaterialIndex >= 0)
+	{
+		using namespace std::string_literals;
+		auto& material = *pMaterials[mesh.mMaterialIndex];
+		aiString texFileName;
+		material.GetTexture(aiTextureType_DIFFUSE, 0, &texFileName);
+		bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx, Surface::FromFile("Models\\nano_textured\\"s + texFileName.C_Str())));
+		bindablePtrs.push_back(std::make_unique<Bind::Sampler>(gfx));
+	}
 
 
 	bindablePtrs.push_back(std::make_unique<VertexBuffer>(gfx, vbuf));

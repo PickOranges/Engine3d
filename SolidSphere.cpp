7a7a4348bc4fc6920d41fea32d5f4a170e1cfbb2
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-21 10:56:43 +0100

    add dynamic vertex system into SolidSphere class.

--- SolidSphere.cpp
+++ SolidSphere.cpp
@@ -2,6 +2,7 @@
 #include "BindableBase.h"
 #include "GraphicsThrowMacros.h"
 #include "Sphere.h"
+#include "Vertex.h"
 
 
 SolidSphere::SolidSphere(Graphics& gfx, float radius)
@@ -9,38 +10,28 @@ SolidSphere::SolidSphere(Graphics& gfx, float radius)
 	namespace dx = DirectX;
 	using namespace Bind;
 
-		struct Vertex
-		{
-			dx::XMFLOAT3 pos;
-		};
-		auto model = Sphere::Make<Vertex>();
-		model.Transform(dx::XMMatrixScaling(radius, radius, radius));
-		AddBind(std::make_shared<VertexBuffer>(gfx, model.vertices));
-		AddBind(std::make_shared<IndexBuffer>(gfx, model.indices));
+	auto model = Sphere::Make();
+	AddBind(std::make_shared<VertexBuffer>(gfx, model.vertices));
+	AddBind(std::make_shared<IndexBuffer>(gfx, model.indices));
 
-		auto pvs = std::make_shared<VertexShader>(gfx, L"SolidVS.cso");
-		auto pvsbc = pvs->GetBytecode();
-		AddBind(std::move(pvs));
+	auto pvs = std::make_shared<VertexShader>(gfx, L"SolidVS.cso");
+	auto pvsbc = pvs->GetBytecode();
+	AddBind(std::move(pvs));
 
-		AddBind(std::make_shared<PixelShader>(gfx, L"SolidPS.cso"));
-		struct PSColorConstant
-		{
-			dx::XMFLOAT3 color = { 1.0f,1.0f,1.0f };
-			float padding;
-		}colorConst;
+	AddBind(std::make_shared<PixelShader>(gfx, L"SolidPS.cso"));
+	struct PSColorConstant
+	{
+		dx::XMFLOAT3 color = { 1.0f,1.0f,1.0f };
+		float padding;
+	}colorConst;
 
-		AddBind(std::make_shared<PixelConstantBuffer<PSColorConstant>>(gfx, colorConst));
+	AddBind(std::make_shared<PixelConstantBuffer<PSColorConstant>>(gfx, colorConst));
 
-		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
-		{
-			{ "Position",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
-		};
-		AddBind(std::make_shared<InputLayout>(gfx, ied, pvsbc));
+	AddBind(std::make_shared<InputLayout>(gfx, model.vertices.GetLayout().GetD3DLayout(), pvsbc));
 
-		AddBind(std::make_shared<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+	AddBind(std::make_shared<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
 
-		AddBind(std::make_shared<TransformCbuf>(gfx, *this));
-
+	AddBind(std::make_shared<TransformCbuf>(gfx, *this));
 }
 
 void SolidSphere::SetPos(DirectX::XMFLOAT3 pos) noexcept
--- Sphere.h
+++ Sphere.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <optional>
+#include "Vertex.h"
 #include "IndexedTriangleList.h"
 #include <DirectXMath.h>
 #include "SimpleMath.h"
@@ -6,8 +8,7 @@
 class Sphere
 {
 public:
-	template<class V>
-	static IndexedTriangleList<V> MakeTesselated(int latDiv, int longDiv)
+	static IndexedTriangleList MakeTesselated(hw3d::VertexLayout layout, int latDiv, int longDiv)
 	{
 		namespace dx = DirectX;
 		assert(latDiv >= 3);
@@ -18,7 +19,7 @@ public:
 		const float lattitudeAngle = PI / latDiv;
 		const float longitudeAngle = 2.0f * PI / longDiv;
 
-		std::vector<V> vertices;
+		hw3d::VertexBuffer vb{ std::move(layout) };
 		for (int iLat = 1; iLat < latDiv; iLat++)
 		{
 			const auto latBase = dx::XMVector3Transform(
@@ -27,25 +28,33 @@ public:
 			);
 			for (int iLong = 0; iLong < longDiv; iLong++)
 			{
-				vertices.emplace_back();
+				dx::XMFLOAT3 calculatedPos;
 				auto v = dx::XMVector3Transform(
 					latBase,
 					dx::XMMatrixRotationZ(longitudeAngle * iLong)
 				);
-				dx::XMStoreFloat3(&vertices.back().pos, v);
+				dx::XMStoreFloat3(&calculatedPos, v);
+				vb.EmplaceBack(calculatedPos);
 			}
 		}
 
 		// add the cap vertices
-		const auto iNorthPole = (unsigned short)vertices.size();
-		vertices.emplace_back();
-		dx::XMStoreFloat3(&vertices.back().pos, base);
-		const auto iSouthPole = (unsigned short)vertices.size();
-		vertices.emplace_back();
-		dx::XMStoreFloat3(&vertices.back().pos, dx::XMVectorNegate(base));
+		const auto iNorthPole = (unsigned short)vb.Size();
+		{
+			dx::XMFLOAT3 northPos;
+			dx::XMStoreFloat3(&northPos, base);
+			vb.EmplaceBack(northPos);
+		}
+		const auto iSouthPole = (unsigned short)vb.Size();
+		{
+			dx::XMFLOAT3 southPos;
+			dx::XMStoreFloat3(&southPos, dx::XMVectorNegate(base));
+			vb.EmplaceBack(southPos);
+		}
 
 		const auto calcIdx = [latDiv, longDiv](unsigned short iLat, unsigned short iLong)
 		{ return iLat * longDiv + iLong; };
+
 		std::vector<unsigned short> indices;
 		for (unsigned short iLat = 0; iLat < latDiv - 2; iLat++)
 		{
@@ -89,11 +98,16 @@ public:
 		indices.push_back(calcIdx(latDiv - 2, longDiv - 1));
 		indices.push_back(iSouthPole);
 
-		return { std::move(vertices),std::move(indices) };
+		return { std::move(vb),std::move(indices) };
 	}
-	template<class V>
-	static IndexedTriangleList<V> Make()
+
+	static IndexedTriangleList Make(std::optional<hw3d::VertexLayout> layout = std::nullopt)
 	{
-		return MakeTesselated<V>(12, 24);
+		using Element = hw3d::VertexLayout::ElementType;
+		if (!layout)
+		{
+			layout = hw3d::VertexLayout{}.Append(Element::Position3D);
+		}
+		return MakeTesselated(std::move(*layout), 12, 24);
 	}
 };

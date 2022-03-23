cffc87645a83e00ad8052c480d750dd5689fe7c8
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-23 10:23:54 +0100

    Forget to bind normal map.

--- TestPlane.cpp
+++ TestPlane.cpp
@@ -15,13 +15,13 @@ TestPlane::TestPlane(Graphics& gfx, float size)
 	AddBind(IndexBuffer::Resolve(gfx, geometryTag, model.indices));
 
 	AddBind(Texture::Resolve(gfx, "Images\\brickwall.jpg"));
+	AddBind(Texture::Resolve(gfx, "Images\\brickwall_normal.jpg",1u));
 
 	auto pvs = VertexShader::Resolve(gfx, "PhongVS.cso");
 	auto pvsbc = pvs->GetBytecode();
 	AddBind(std::move(pvs));
 
 	AddBind(PixelShader::Resolve(gfx, "PhongPSNormalMap.cso"));
-	//AddBind(PixelShader::Resolve(gfx, "PhongPS.cso"));
 
 	AddBind(PixelConstantBuffer<PSMaterialConstant>::Resolve(gfx, pmc, 1u));
 

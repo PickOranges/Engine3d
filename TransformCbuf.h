ad7b0b01ecd5b18835120e9dfd6c8bce5dfd8a23
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-23 11:59:23 +0100

    Create TransformCbufDual for binding TF on both VertexShader and PixelShader

--- TransformCbuf.h
+++ TransformCbuf.h
@@ -24,4 +24,18 @@ namespace Bind {
 		static std::unique_ptr<VertexConstantBuffer<Transforms>> pVcbuf;
 		const Drawable& parent;
 	};
+
+
+
+	// This class is for binding TF on both VertexShader and PixelShader
+	class TransformCbufDual : public TransformCbuf
+	{
+	public:
+		TransformCbufDual(Graphics& gfx, const Drawable& parent, UINT slotV = 0u, UINT slotP = 0u);
+		void Bind(Graphics& gfx) noexcept override;
+	protected:
+		void UpdateBindImpl(Graphics& gfx, const Transforms& tf) noexcept;
+	private:
+		static std::unique_ptr<PixelConstantBuffer<Transforms>> pPcbuf;
+	};
 }

06e32ee8c84f97d24a7003aad030ca446bcfcfe5
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-26 14:47:39 +0100

    Add TexturePreprocessor for preprocessing via command line.

---
+++ TexturePreprocessor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Surface.h"
+#include <string>
+#include <DirectXMath.h>
+
+
+class TexturePreprocessor
+{
+public:
+	static void FlipYAllNormalMapsInObj(const std::string& objPath);
+	static void FlipYNormalMap(const std::string& pathIn, const std::string& pathOut);
+private:
+	template<typename F>
+	static void TransformFile(const std::string& pathIn, const std::string& pathOut, F&& func);
+	static DirectX::XMVECTOR ColorToVector(Surface::Color c) noexcept;
+	static Surface::Color VectorToColor(DirectX::FXMVECTOR n) noexcept;
+};

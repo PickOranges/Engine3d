61bdbf569ebf6eb7860a7448c0344a49cad59a87
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-03-26 12:28:24 +0100

    Adjust frustum far plane.

--- App.cpp
+++ App.cpp
@@ -8,7 +8,7 @@ App::App()
 	wnd(1280, 720, "Test App Class Obj"),
 	light(wnd.Gfx())
 {
-	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 9.0f / 16.0f, 0.5f, 40.0f));
+	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 9.0f / 16.0f, 0.5f, 400.0f));
 }
 
 int App::Go()

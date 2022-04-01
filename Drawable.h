71c35a7d6689806fb5e170417f7a634c8eb5a706
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-01 13:31:52 +0200

    update

--- Drawable.h
+++ Drawable.h
@@ -20,23 +20,18 @@ class Drawable
 {
 public:
 	Drawable() = default;
-	Drawable(Graphics& gfx, const Material& mat, const aiMesh& mesh) noexcept;
+	Drawable(Graphics & gfx, const Material & mat, const aiMesh & mesh) noexcept;
 	Drawable(const Drawable&) = delete;
 	void AddTechnique(Technique tech_in) noexcept;
 	virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
 	void Submit(class FrameCommander& frame) const noexcept;
-	void Bind(Graphics& gfx) const noexcept;
-	void Accept(TechniqueProbe& probe);
+	void Bind(Graphics & gfx) const noexcept;
+	void Accept(TechniqueProbe & probe);
 	UINT GetIndexCount() const noexcept(!IS_DEBUG);
 	virtual ~Drawable();
-
 protected:
 	std::shared_ptr<Bind::IndexBuffer> pIndices;
 	std::shared_ptr<Bind::VertexBuffer> pVertices;
 	std::shared_ptr<Bind::Topology> pTopology;
 	std::vector<Technique> techniques;
-
-private:
-	const Bind::IndexBuffer* pIndexBuffer = nullptr;
-	std::vector<std::shared_ptr<Bind::Bindable>> binds;
 };

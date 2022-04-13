82370c331e601075758e812406fd1456d18e3f2b
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-13 12:38:14 +0200

    constrast for debugging normal map

--- App.cpp
+++ App.cpp
@@ -9,7 +9,6 @@ App::App()
 	wnd(1280, 720, "Test App Class Obj"),
 	light(wnd.Gfx())
 {
-	TestMaterialSystemLoading(wnd.Gfx());
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 9.0f / 16.0f, 0.5f, 400.0f));
 	cube.SetPos({ 4.0f,0.0f,0.0f });
 	cube2.SetPos({ 0.0f,4.0f,0.0f });
@@ -57,8 +56,8 @@ void App::DoFrame()
 
 
 	light.Submit(fc);
-	//cube.Submit(fc);
-	//cube2.Submit(fc);
+	cube.Submit(fc);
+	cube2.Submit(fc);
 	pLoaded->Submit(fc, DirectX::XMMatrixIdentity());
 	fc.Execute(wnd.Gfx());
 

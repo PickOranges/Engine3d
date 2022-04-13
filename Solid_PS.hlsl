5f426e2888a120fdf4edf5869e875285e488c9f4
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-14 00:42:46 +0200

    Add dyn control for brick_wall model.

--- Solid_PS.hlsl
+++ Solid_PS.hlsl
@@ -1,10 +1,11 @@
 // for point light
 cbuffer CBuf:register(b1)
 {
-	float3 materialColor;
+	float3 color;
 };
 
 float4 main() : SV_Target
 {
-	return float4(materialColor, 1.0f);
+	//return color;
+	return float4(color, 1.0f);
 }

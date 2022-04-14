c14f8c6c1073e0ec3e018e9d1e30214c6f3b3f76
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-14 16:15:13 +0200

    improve dyn controlling of sponza.
    issue: test cubes still have problem with pixel shader slot const. buffer.

--- App.h
+++ App.h
@@ -47,8 +47,8 @@ private:
 
 
 	Model sponza{ wnd.Gfx(),"models\\sponza\\sponza.obj",1.0f/20.f };
-	TestCube cube{ wnd.Gfx(),4.0f }; 
-	TestCube cube2{ wnd.Gfx(),4.0f };
+	//TestCube cube{ wnd.Gfx(),4.0f }; 
+	//TestCube cube2{ wnd.Gfx(),4.0f };
 	//std::unique_ptr<Mesh> pLoaded;
 };
 

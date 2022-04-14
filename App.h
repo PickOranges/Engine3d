b94510ee040cdb0e92d361949166d8af1573cdf9
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-14 08:57:12 +0200

    Fix the scaling problem, but the sponza scene still weird.

--- App.h
+++ App.h
@@ -46,7 +46,7 @@ private:
 	bool showDemoWindow = false;
 
 
-	Model sponza{ wnd.Gfx(),"models\\sponza\\sponza.obj",1.0f / 20.0f };
+	Model sponza{ wnd.Gfx(),"models\\sponza\\sponza.obj",1.0f/20.f };
 	//TestCube cube{ wnd.Gfx(),4.0f }; 
 	//TestCube cube2{ wnd.Gfx(),4.0f };
 	//std::unique_ptr<Mesh> pLoaded;

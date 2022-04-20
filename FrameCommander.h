f61a3a39c61be409113ffee8a89203e3ec21b05f
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-20 12:25:43 +0200

    got stuck

--- FrameCommander.h
+++ FrameCommander.h
@@ -97,3 +97,42 @@ private:
 	std::shared_ptr<Bind::InputLayout> pLayoutFull;
 	std::shared_ptr<Bind::Sampler> pSamplerFull;
 };
+
+
+
+
+
+
+//class FrameCommander
+//{
+//public:
+//	void Accept(Job job, size_t target) noexcept
+//	{
+//		passes[target].Accept(job);
+//	}
+//	void Execute(Graphics& gfx) const noexcept(!IS_DEBUG)
+//	{
+//		using namespace Bind;
+//
+//
+//		// main phong lighting pass
+//		Stencil::Resolve(gfx, Stencil::Mode::Off)->Bind(gfx);
+//		passes[0].Execute(gfx);
+//		// outline masking pass
+//		Stencil::Resolve(gfx, Stencil::Mode::Write)->Bind(gfx);
+//		NullPixelShader::Resolve(gfx)->Bind(gfx);
+//		passes[1].Execute(gfx);
+//		// outline drawing pass
+//		Stencil::Resolve(gfx, Stencil::Mode::Mask)->Bind(gfx);
+//		passes[2].Execute(gfx);
+//	}
+//	void Reset() noexcept
+//	{
+//		for (auto& p : passes)
+//		{
+//			p.Reset();
+//		}
+//	}
+//private:
+//	std::array<Pass, 3> passes;
+//};

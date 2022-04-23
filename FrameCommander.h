6802059e028b61f60adc62a7fa8f5fa1d9e6a70e
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-04-23 23:25:12 +0200

    debug: recover the old effect, but with the new FrameCommander ctor.

--- FrameCommander.h
+++ FrameCommander.h
@@ -16,25 +16,25 @@ public:
 		ds(gfx, gfx.GetWidth(), gfx.GetHeight()),
 		rt(gfx, gfx.GetWidth(), gfx.GetHeight())
 	{
-		namespace dx = DirectX;
+		//namespace dx = DirectX;
 
-		// setup fullscreen geometry
-		hw3d::VertexLayout lay;
-		lay.Append(hw3d::VertexLayout::Position2D);
-		hw3d::VertexBuffer bufFull{ lay };
-		bufFull.EmplaceBack(dx::XMFLOAT2{ -1,1 });
-		bufFull.EmplaceBack(dx::XMFLOAT2{ 1,1 });
-		bufFull.EmplaceBack(dx::XMFLOAT2{ -1,-1 });
-		bufFull.EmplaceBack(dx::XMFLOAT2{ 1,-1 });
-		pVbFull = Bind::VertexBuffer::Resolve(gfx, "$Full", std::move(bufFull));
-		std::vector<unsigned short> indices = { 0,1,2,1,3,2 };
-		pIbFull = Bind::IndexBuffer::Resolve(gfx, "$Full", std::move(indices));
+		//// setup fullscreen geometry
+		//hw3d::VertexLayout lay;
+		//lay.Append(hw3d::VertexLayout::Position2D);
+		//hw3d::VertexBuffer bufFull{ lay };
+		//bufFull.EmplaceBack(dx::XMFLOAT2{ -1,1 });
+		//bufFull.EmplaceBack(dx::XMFLOAT2{ 1,1 });
+		//bufFull.EmplaceBack(dx::XMFLOAT2{ -1,-1 });
+		//bufFull.EmplaceBack(dx::XMFLOAT2{ 1,-1 });
+		//pVbFull = Bind::VertexBuffer::Resolve(gfx, "$Full", std::move(bufFull));
+		//std::vector<unsigned short> indices = { 0,1,2,1,3,2 };
+		//pIbFull = Bind::IndexBuffer::Resolve(gfx, "$Full", std::move(indices));
 
-		// setup fullscreen shaders
-		pPsFull = Bind::PixelShader::Resolve(gfx, "TEST_GaussBlurBruteForce_PS.cso");
-		pVsFull = Bind::VertexShader::Resolve(gfx, "TEST_Fullscreen_VS.cso");
-		pLayoutFull = Bind::InputLayout::Resolve(gfx, lay, pVsFull->GetBytecode());
-		pSamplerFull = Bind::Sampler::Resolve(gfx, false, true);
+		//// setup fullscreen shaders
+		//pPsFull = Bind::PixelShader::Resolve(gfx, "TEST_GaussBlurBruteForce_PS.cso");
+		//pVsFull = Bind::VertexShader::Resolve(gfx, "TEST_Fullscreen_VS.cso");
+		//pLayoutFull = Bind::InputLayout::Resolve(gfx, lay, pVsFull->GetBytecode());
+		//pSamplerFull = Bind::Sampler::Resolve(gfx, false, true);
 	}
 
 
@@ -52,8 +52,8 @@ public:
 
 		// setup render target used for normal passes
 		ds.Clear(gfx);
-		//gfx.BindSwapBuffer(ds);
-		rt.BindAsTarget(gfx, ds);
+		gfx.BindSwapBuffer(ds);
+		//rt.BindAsTarget(gfx, ds);
 
 		// main phong lighting pass
 		Stencil::Resolve(gfx, Stencil::Mode::Off)->Bind(gfx);
@@ -64,18 +64,19 @@ public:
 		passes[1].Execute(gfx);
 		// outline drawing pass
 		Stencil::Resolve(gfx, Stencil::Mode::Mask)->Bind(gfx);
+		//Stencil::Resolve(gfx, Stencil::Mode::Off)->Bind(gfx);
 		passes[2].Execute(gfx);
 
-		// fullscreen blur pass
-		gfx.BindSwapBuffer();
-		rt.BindAsTexture(gfx, 0);
-		pVbFull->Bind(gfx);
-		pIbFull->Bind(gfx);
-		pVsFull->Bind(gfx);
-		pPsFull->Bind(gfx);
-		pLayoutFull->Bind(gfx);
-		pSamplerFull->Bind(gfx);
-		gfx.DrawIndexed(pIbFull->GetCount());
+		//// fullscreen blur pass
+		//gfx.BindSwapBuffer();
+		//rt.BindAsTexture(gfx, 0);
+		//pVbFull->Bind(gfx);
+		//pIbFull->Bind(gfx);
+		//pVsFull->Bind(gfx);
+		//pPsFull->Bind(gfx);
+		//pLayoutFull->Bind(gfx);
+		//pSamplerFull->Bind(gfx);
+		//gfx.DrawIndexed(pIbFull->GetCount());
 	}
 	void Reset() noexcept
 	{
@@ -97,42 +98,3 @@ private:
 	std::shared_ptr<Bind::InputLayout> pLayoutFull;
 	std::shared_ptr<Bind::Sampler> pSamplerFull;
 };
-
-
-
-
-
-
-//class FrameCommander
-//{
-//public:
-//	void Accept(Job job, size_t target) noexcept
-//	{
-//		passes[target].Accept(job);
-//	}
-//	void Execute(Graphics& gfx) const noexcept(!IS_DEBUG)
-//	{
-//		using namespace Bind;
-//
-//
-//		// main phong lighting pass
-//		Stencil::Resolve(gfx, Stencil::Mode::Off)->Bind(gfx);
-//		passes[0].Execute(gfx);
-//		// outline masking pass
-//		Stencil::Resolve(gfx, Stencil::Mode::Write)->Bind(gfx);
-//		NullPixelShader::Resolve(gfx)->Bind(gfx);
-//		passes[1].Execute(gfx);
-//		// outline drawing pass
-//		Stencil::Resolve(gfx, Stencil::Mode::Mask)->Bind(gfx);
-//		passes[2].Execute(gfx);
-//	}
-//	void Reset() noexcept
-//	{
-//		for (auto& p : passes)
-//		{
-//			p.Reset();
-//		}
-//	}
-//private:
-//	std::array<Pass, 3> passes;
-//};

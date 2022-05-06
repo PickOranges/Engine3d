08d1f42a67c91ee54969dedcd33cc1ca5ad7ef8a
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-06 21:28:42 +0200

    udapte

--- TEST_GaussBlurBruteForce_PS.hlsl
+++ TEST_GaussBlurBruteForce_PS.hlsl
@@ -12,18 +12,16 @@ float4 main(float2 uv : Texcoord) : SV_Target
     tex.GetDimensions(width, height);
     const float dx = 1.0f / width;
     const float dy = 1.0f / height;
-    float accAlpha = 0.0f;
-    float3 maxColor = float3(0.0f, 0.0f, 0.0f);
+    float4 acc = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    
     for (int y = -r; y <= r; y++)
     {
         for (int x = -r; x <= r; x++)
         {
             const float2 tc = uv + float2(dx * x, dy * y);
             
-            const float4 s = tex.Sample(splr, tc).rgba;
-            accAlpha += s.a;
-            maxColor = max(s.rgb, maxColor);
+            acc += tex.Sample(splr, tc).rgba;
         }
     }
-    return float4(maxColor, accAlpha / divisor);
+    return acc / divisor;
 }

3c98711b6de5f7dd73650a26f45d86e096b3bd15
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-06 23:20:42 +0200

    Imgui window: two different mode of outline.

--- BlurPack.h
+++ BlurPack.h
@@ -14,7 +14,7 @@ public:
 		radius(radius),
 		sigma(sigma)
 	{
-		SetKernel(gfx, radius, sigma);
+		SetKernelGauss(gfx, radius, sigma);
 	}
 	void Bind(Graphics& gfx) noexcept
 	{
@@ -30,27 +30,61 @@ public:
 	{
 		ccb.Update(gfx, { FALSE });
 	}
-
-	void ShowWindow(Graphics& gfx)
+	void RenderWidgets(Graphics& gfx)
 	{
-		ImGui::Begin("Blur");
+		bool filterChanged = false;
+		{
+			const char* items[] = { "Gauss","Box" };
+			static const char* curItem = items[0];
+			if (ImGui::BeginCombo("Filter Type", curItem))
+			{
+				for (int n = 0; n < std::size(items); n++)
+				{
+					const bool isSelected = (curItem == items[n]);
+					if (ImGui::Selectable(items[n], isSelected))
+					{
+						filterChanged = true;
+						curItem = items[n];
+						if (curItem == items[0])
+						{
+							kernelType = KernelType::Gauss;
+						}
+						else if (curItem == items[1])
+						{
+							kernelType = KernelType::Box;
+						}
+					}
+					if (isSelected)
+					{
+						ImGui::SetItemDefaultFocus();
+					}
+				}
+				ImGui::EndCombo();
+			}
+		}
+
 		bool radChange = ImGui::SliderInt("Radius", &radius, 0, 15);
 		bool sigChange = ImGui::SliderFloat("Sigma", &sigma, 0.1f, 10.0f);
-		if (radChange || sigChange)
+		if (radChange || sigChange || filterChanged)
 		{
-			SetKernel(gfx, radius, sigma);
+			if (kernelType == KernelType::Gauss)
+			{
+				SetKernelGauss(gfx, radius, sigma);
+			}
+			else if (kernelType == KernelType::Box)
+			{
+				SetKernelBox(gfx, radius);
+			}
 		}
-		ImGui::End();
 	}
-	// nTaps should be 6sigma - 1
 	// for more accurate coefs, need to integrate, but meh :/
-	void SetKernel(Graphics& gfx, int radius, float sigma) noexcept(!IS_DEBUG)
+	void SetKernelGauss(Graphics& gfx, int radius, float sigma) noexcept(!IS_DEBUG)
 	{
-		assert(radius <= 15);
+		assert(radius <= maxRadius);
 		Kernel k;
 		k.nTaps = radius * 2 + 1;
 		float sum = 0.0f;
-		for (int i = 1; i < k.nTaps; i++)
+		for (int i = 0; i < k.nTaps; i++)
 		{
 			const auto x = float(i - radius);
 			const auto g = gauss(x, sigma);
@@ -63,14 +97,33 @@ public:
 		}
 		pcb.Update(gfx, k);
 	}
+	void SetKernelBox(Graphics& gfx, int radius) noexcept(!IS_DEBUG)
+	{
+		assert(radius <= maxRadius);
+		Kernel k;
+		k.nTaps = radius * 2 + 1;
+		const float c = 1.0f / k.nTaps;
+		for (int i = 0; i < k.nTaps; i++)
+		{
+			k.coefficients[i].x = c;
+		}
+		pcb.Update(gfx, k);
+	}
 private:
+	enum class KernelType
+	{
+		Gauss,
+		Box
+	};
+	static constexpr int maxRadius = 15;
 	int radius;
 	float sigma;
+	KernelType kernelType = KernelType::Gauss;
 	struct Kernel
 	{
 		int nTaps;
 		float padding[3];
-		DirectX::XMFLOAT4 coefficients[31];
+		DirectX::XMFLOAT4 coefficients[maxRadius * 2 + 1];
 	};
 	struct Control
 	{

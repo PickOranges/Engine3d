7c8975a00db8d7fa4d58a2bfb9055ed7ba31e12d
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-06 22:17:11 +0200

    udpate

--- BlurPack.h
+++ BlurPack.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "BindableBase.h"
 #include "SimpleMath.h"
+#include "imgui/imgui.h"
 
 class BlurPack
 {
 public:
-	BlurPack(Graphics& gfx, int radius = 7, float sigma = 2.6f)
+	BlurPack(Graphics& gfx, int radius = 7, float sigma = 2.6f, const char* shader = "Blur_PS.cso")
 		:
-		shader(gfx, "Blur_PS.cso"),
+		shader(gfx, shader),
 		pcb(gfx, 0u),
-		ccb(gfx, 1u)
+		ccb(gfx, 1u),
+		radius(radius),
+		sigma(sigma)
 	{
 		SetKernel(gfx, radius, sigma);
 	}
@@ -27,11 +30,23 @@ public:
 	{
 		ccb.Update(gfx, { FALSE });
 	}
+
+	void ShowWindow(Graphics& gfx)
+	{
+		ImGui::Begin("Blur");
+		bool radChange = ImGui::SliderInt("Radius", &radius, 0, 15);
+		bool sigChange = ImGui::SliderFloat("Sigma", &sigma, 0.1f, 10.0f);
+		if (radChange || sigChange)
+		{
+			SetKernel(gfx, radius, sigma);
+		}
+		ImGui::End();
+	}
 	// nTaps should be 6sigma - 1
 	// for more accurate coefs, need to integrate, but meh :/
 	void SetKernel(Graphics& gfx, int radius, float sigma) noexcept(!IS_DEBUG)
 	{
-		assert(radius <= 7);
+		assert(radius <= 15);
 		Kernel k;
 		k.nTaps = radius * 2 + 1;
 		float sum = 0.0f;
@@ -49,11 +64,13 @@ public:
 		pcb.Update(gfx, k);
 	}
 private:
+	int radius;
+	float sigma;
 	struct Kernel
 	{
 		int nTaps;
 		float padding[3];
-		DirectX::XMFLOAT4 coefficients[15];
+		DirectX::XMFLOAT4 coefficients[31];
 	};
 	struct Control
 	{

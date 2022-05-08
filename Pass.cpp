bb62833e9c61492d07ca78e98b816489d1177ae3
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-08 18:39:37 +0200

    update

--- Pass.cpp
+++ Pass.cpp
@@ -8,16 +8,54 @@
 #include "Source.h"
 
 
+namespace Rgph
+{
+	Pass::Pass(std::string name) noexcept
+		:
+		name(std::move(name))
+	{}
+
+	void Pass::Reset() noexcept(!IS_DEBUG)
+	{}
+
+	const std::string& Pass::GetName() const noexcept
 	{
-	if (src->GetName() == name)
-	{
-		return *src;
+		return name;
 	}
+
+	void Pass::Finalize()
+	{
+		for (auto& in : sinks)
+		{
+			in->PostLinkValidate();
+		}
+		for (auto& out : sources)
+		{
+			out->PostLinkValidate();
+		}
+	}
+
+	Pass::~Pass()
+	{}
+
+	const std::vector<std::unique_ptr<Sink>>& Pass::GetSinks() const
+	{
+		return sinks;
+	}
+
+	Source& Pass::GetSource(const std::string& name) const
+	{
+		for (auto& src : sources)
+		{
+			if (src->GetName() == name)
+			{
+				return *src;
+			}
 		}
 
-	std::ostringstream oss;
-	oss << "Output named [" << name << "] not found in pass: " << GetName();
-	throw RGC_EXCEPTION(oss.str());
+		std::ostringstream oss;
+		oss << "Output named [" << name << "] not found in pass: " << GetName();
+		throw RGC_EXCEPTION(oss.str());
 	}
 
 	Sink& Pass::GetSink(const std::string& registeredName) const
@@ -72,17 +110,5 @@
 			throw RGC_EXCEPTION("Input target has incorrect format");
 		}
 		sink.SetTarget(std::move(targetSplit[0]), std::move(targetSplit[1]));
-	}
-
-	void Pass::BindBufferResources(Graphics& gfx) const noxnd
-	{
-		if (renderTarget)
-		{
-			renderTarget->BindAsBuffer(gfx, depthStencil.get());
-		}
-		else
-		{
-			depthStencil->BindAsBuffer(gfx);
-		}
 	}
 }

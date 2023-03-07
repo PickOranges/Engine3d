656017e311279de8f94051c83263b4c8b83cbf8b
chengzimdl@gmail.com <chengzimdl@gmail.com>
2023-03-07 09:43:51 +0100

    create a new branch for SM development

--- FrameCommander.h
+++ FrameCommander.h
@@ -21,24 +21,6 @@ public:
 	{
 		namespace dx = DirectX;
 
-		// setup fullscreen geometry
-		hw3d::VertexLayout lay;
-		lay.Append(hw3d::VertexLayout::Position2D);
-		hw3d::VertexBuffer bufFull{ lay };
-		bufFull.EmplaceBack(dx::XMFLOAT2{ -1,1 });
-		bufFull.EmplaceBack(dx::XMFLOAT2{ 1,1 });
-		bufFull.EmplaceBack(dx::XMFLOAT2{ -1,-1 });
-		bufFull.EmplaceBack(dx::XMFLOAT2{ 1,-1 });
-		pVbFull = Bind::VertexBuffer::Resolve(gfx, "$Full", std::move(bufFull));
-		std::vector<unsigned short> indices = { 0,1,2,1,3,2 };
-		pIbFull = Bind::IndexBuffer::Resolve(gfx, "$Full", std::move(indices));
-
-		// setup fullscreen shaders
-		pVsFull = Bind::VertexShader::Resolve(gfx, "Fullscreen_VS.cso");
-		pLayoutFull = Bind::InputLayout::Resolve(gfx, lay, pVsFull->GetBytecode());
-		pSamplerFullPoint = Bind::Sampler::Resolve(gfx, false, true);
-		pSamplerFullBilin = Bind::Sampler::Resolve(gfx, true, true);
-		pBlenderMerge = Bind::Blender::Resolve(gfx, true);
 	}
 	void Accept(Job job, size_t target) noexcept
 	{
@@ -67,25 +49,7 @@ public:
 		rt1->BindAsTarget(gfx);
 		Stencil::Resolve(gfx, Stencil::Mode::Off)->Bind(gfx);
 		passes[2].Execute(gfx);
-		// fullscreen blur h-pass
-		rt2->BindAsTarget(gfx);
-		rt1->BindAsTexture(gfx, 0);
-		pVbFull->Bind(gfx);
-		pIbFull->Bind(gfx);
-		pVsFull->Bind(gfx);
-		pLayoutFull->Bind(gfx);
-		pSamplerFullPoint->Bind(gfx);
-		blur.Bind(gfx);
-		blur.SetHorizontal(gfx);
-		gfx.DrawIndexed(pIbFull->GetCount());
-		// fullscreen blur v-pass + combine
-		gfx.BindSwapBuffer(ds);
-		rt2->BindAsTexture(gfx, 0u);
-		pBlenderMerge->Bind(gfx);
-		pSamplerFullBilin->Bind(gfx);
-		Stencil::Resolve(gfx, Stencil::Mode::Mask)->Bind(gfx);
-		blur.SetVertical(gfx);
-		gfx.DrawIndexed(pIbFull->GetCount());
+
 	}
 	void Reset() noexcept
 	{

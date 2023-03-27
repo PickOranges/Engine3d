ce307868421fa6001a89bd25d32a8a25cf48e030
chengzimdl@gmail.com <chengzimdl@gmail.com>
2023-03-27 13:52:35 +0200

    separate the ImGui controls of camera with the active camera. Thus we can adjust any camera's params, no matter whether it active is.

--- Projection.cpp
+++ Projection.cpp
@@ -9,7 +9,11 @@ Projection::Projection(Graphics& gfx, float width, float height, float nearZ, fl
 	height(height),
 	nearZ(nearZ),
 	farZ(farZ),
-	frust(gfx, width, height, nearZ, farZ)
+	frust(gfx, width, height, nearZ, farZ), 
+	homeWidth(width), 
+	homeHeight(height), 
+	homeNearZ(nearZ), 
+	homeFarZ(farZ)
 {}
 
 DirectX::XMMATRIX Projection::GetMatrix() const
@@ -21,11 +25,11 @@ void Projection::RenderWidgets(Graphics& gfx)
 {
 	bool dirty = false;
 	const auto dcheck = [&dirty](bool d) { dirty = dirty || d; };
-
+	ImGui::Text("Projection");
 	dcheck(ImGui::SliderFloat("Width", &width, 0.01f, 4.0f, "%.2f", 1.f));
 	dcheck(ImGui::SliderFloat("Height", &height, 0.01f, 4.0f, "%.2f", 1.f));
-	dcheck(ImGui::SliderFloat("Near Z", &nearZ, 0.01f, 400.0f, "%.2f", 1.f));
-	dcheck(ImGui::SliderFloat("Far Z", &farZ, 0.01f, 400.0f, "%.2f", 1.f));
+	dcheck(ImGui::SliderFloat("Near Z", &nearZ, 0.01f, farZ - 0.01f, "%.2f", 1.f));
+	dcheck(ImGui::SliderFloat("Far Z", &farZ, 0.01f, nearZ + 0.01f, "%.2f", 1.f));
 
 	if (dirty)
 	{
@@ -51,4 +55,13 @@ void Projection::Submit() const
 void Projection::LinkTechniques(Rgph::RenderGraph& rg)
 {
 	frust.LinkTechniques(rg);
+}
+
+void Projection::Reset(Graphics& gfx)
+{
+	width = homeWidth;
+	height = homeHeight;
+	nearZ = homeNearZ;
+	farZ = homeFarZ;
+	frust.SetVertices(gfx, width, height, nearZ, farZ);
 }
